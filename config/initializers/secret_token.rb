067428bacdd1215d562c7a3c7b9a2f5ba87a3236
Eugene Munyaneza <munyanezae@gmail.com>
2012-10-02 15:32:05 +0200

    Initial setup of the project, nothing yet added functional wise.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iwacu::Application.config.secret_token = '140b066a359a116c9936b97a287632b952e8d18455b699b61e2975c099ae0002449dd9edf034d04bf00269d9687cb037afe49ae435093d319b233e5fa3a91737'
